d54507e6d75afdf83325947387dbc08470a8d173
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-18 00:12:02 -0400

    dynamic search for part of string

--- app/controllers/api/stock_prices_controller.rb
+++ app/controllers/api/stock_prices_controller.rb
@@ -1,6 +1,12 @@
 class Api::StockPricesController < ApplicationController
   def index
-    @stock_prices = StockPrice.where(name: params[:name])
+    @stock_prices = StockPrice.all
+    @search_term = []
+    @stock_prices.each do |stock_price|
+      if stock_price["name"].downcase.include? params[:name]
+        @search_term << stock_price
+      end
+    end
     render "index.json.jb"
   end
 end
